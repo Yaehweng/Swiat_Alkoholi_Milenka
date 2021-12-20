092ce28ce430ee5fe479a38b8d8c758bfe2daf4b
Yaehweng <93915880+Yaehweng@users.noreply.github.com>
2021-12-20 14:19:42 +0100

    Add files via upload

---
+++ ŚwiatAlkoholi_MilenaBrowczuk/ŚwiatAlkoholi_MilenaBrowczuk/Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿
+namespace ŚwiatAlkoholi_MilenaBrowczuk
+{
+    partial class Świat_Alkoholi_weryfikacja
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Świat_Alkoholi_weryfikacja));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.imię = new System.Windows.Forms.TextBox();
+            this.nazwisko = new System.Windows.Forms.TextBox();
+            this.adres = new System.Windows.Forms.TextBox();
+            this.nrlokalu = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.wyślij = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.koszyk = new System.Windows.Forms.RichTextBox();
+            this.zamówienie = new System.Windows.Forms.Label();
+            this.lista_produktów = new System.Windows.Forms.ComboBox();
+            this.dodaj = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Maroon;
+            this.label1.Font = new System.Drawing.Font("Ink Free", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.label1.Location = new System.Drawing.Point(160, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(324, 60);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Świat Alkoholi";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.UseWaitCursor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Maroon;
+            this.label2.Font = new System.Drawing.Font("Ink Free", 20.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.label2.Location = new System.Drawing.Point(22, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(591, 34);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Prosimy o wypełnienie formularza w celu weryfikacji";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.UseWaitCursor = true;
+            // 
+            // imię
+            // 
+            this.imię.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.imię.Font = new System.Drawing.Font("Ink Free", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.imię.ForeColor = System.Drawing.Color.Maroon;
+            this.imię.Location = new System.Drawing.Point(136, 289);
+            this.imię.Name = "imię";
+            this.imię.Size = new System.Drawing.Size(172, 31);
+            this.imię.TabIndex = 2;
+            this.imię.Text = "Imię";
+            this.imię.UseWaitCursor = true;
+            // 
+            // nazwisko
+            // 
+            this.nazwisko.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.nazwisko.Font = new System.Drawing.Font("Ink Free", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nazwisko.ForeColor = System.Drawing.Color.Maroon;
+            this.nazwisko.Location = new System.Drawing.Point(136, 343);
+            this.nazwisko.Name = "nazwisko";
+            this.nazwisko.Size = new System.Drawing.Size(172, 31);
+            this.nazwisko.TabIndex = 3;
+            this.nazwisko.Text = "Nazwisko";
+            this.nazwisko.UseWaitCursor = true;
+            // 
+            // adres
+            // 
+            this.adres.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.adres.Font = new System.Drawing.Font("Ink Free", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.adres.ForeColor = System.Drawing.Color.Maroon;
+            this.adres.Location = new System.Drawing.Point(327, 289);
+            this.adres.Name = "adres";
+            this.adres.Size = new System.Drawing.Size(170, 31);
+            this.adres.TabIndex = 4;
+            this.adres.Text = "Adres(nr)";
+            this.adres.UseWaitCursor = true;
+            // 
+            // nrlokalu
+            // 
+            this.nrlokalu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.nrlokalu.Font = new System.Drawing.Font("Ink Free", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nrlokalu.ForeColor = System.Drawing.Color.Maroon;
+            this.nrlokalu.Location = new System.Drawing.Point(327, 343);
+            this.nrlokalu.Name = "nrlokalu";
+            this.nrlokalu.Size = new System.Drawing.Size(172, 31);
+            this.nrlokalu.TabIndex = 5;
+            this.nrlokalu.Text = "nr lokalu(opcjonalnie)";
+            this.nrlokalu.UseWaitCursor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Ink Free", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.label3.Location = new System.Drawing.Point(111, 391);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(197, 26);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Podaj datę urodzenia";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label3.UseWaitCursor = true;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CalendarFont = new System.Drawing.Font("Ink Free", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.CalendarForeColor = System.Drawing.Color.Maroon;
+            this.dateTimePicker1.CalendarMonthBackground = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.dateTimePicker1.CalendarTitleBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.dateTimePicker1.CalendarTitleForeColor = System.Drawing.Color.Maroon;
+            this.dateTimePicker1.CalendarTrailingForeColor = System.Drawing.Color.Firebrick;
+            this.dateTimePicker1.Font = new System.Drawing.Font("Ink Free", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.Location = new System.Drawing.Point(327, 395);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(220, 22);
+            this.dateTimePicker1.TabIndex = 11;
+            this.dateTimePicker1.UseWaitCursor = true;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // wyślij
+            // 
+            this.wyślij.BackColor = System.Drawing.Color.Maroon;
+            this.wyślij.Font = new System.Drawing.Font("Ink Free", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.wyślij.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.wyślij.Location = new System.Drawing.Point(273, 79);
+            this.wyślij.Name = "wyślij";
+            this.wyślij.Size = new System.Drawing.Size(75, 40);
+            this.wyślij.TabIndex = 12;
+            this.wyślij.Text = "Wyślij";
+            this.wyślij.UseVisualStyleBackColor = false;
+            this.wyślij.UseWaitCursor = true;
+            this.wyślij.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.panel1.Controls.Add(this.koszyk);
+            this.panel1.Controls.Add(this.zamówienie);
+            this.panel1.Controls.Add(this.lista_produktów);
+            this.panel1.Controls.Add(this.dodaj);
+            this.panel1.Controls.Add(this.wyślij);
+            this.panel1.Location = new System.Drawing.Point(136, 131);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(363, 132);
+            this.panel1.TabIndex = 13;
+            this.panel1.UseWaitCursor = true;
+            this.panel1.Visible = false;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // koszyk
+            // 
+            this.koszyk.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.koszyk.Font = new System.Drawing.Font("Ink Free", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.koszyk.ForeColor = System.Drawing.Color.Maroon;
+            this.koszyk.Location = new System.Drawing.Point(23, 36);
+            this.koszyk.Name = "koszyk";
+            this.koszyk.Size = new System.Drawing.Size(149, 83);
+            this.koszyk.TabIndex = 16;
+            this.koszyk.Text = "";
+            this.koszyk.UseWaitCursor = true;
+            this.koszyk.TextChanged += new System.EventHandler(this.koszyk_TextChanged);
+            // 
+            // zamówienie
+            // 
+            this.zamówienie.AutoSize = true;
+            this.zamówienie.Font = new System.Drawing.Font("Ink Free", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.zamówienie.ForeColor = System.Drawing.Color.Maroon;
+            this.zamówienie.Location = new System.Drawing.Point(19, 9);
+            this.zamówienie.Name = "zamówienie";
+            this.zamówienie.Size = new System.Drawing.Size(112, 23);
+            this.zamówienie.TabIndex = 15;
+            this.zamówienie.Text = "Zamówienie";
+            this.zamówienie.UseWaitCursor = true;
+            // 
+            // lista_produktów
+            // 
+            this.lista_produktów.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.lista_produktów.Font = new System.Drawing.Font("Ink Free", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lista_produktów.ForeColor = System.Drawing.Color.Maroon;
+            this.lista_produktów.FormattingEnabled = true;
+            this.lista_produktów.Items.AddRange(new object[] {
+            "Jameson Irish Whiskey Triple Distilled 0,3l",
+            "Jameson Irish Whiskey Triple Distilled 0,7l",
+            "Jameson Black Barrel Whiskey 0,7l",
+            "Whiskey Burbon Evan Williams Honey 0,7l",
+            "Whiskey Burbon Woodford Chocolate Malted Rye 0,7l"});
+            this.lista_produktów.Location = new System.Drawing.Point(191, 9);
+            this.lista_produktów.Name = "lista_produktów";
+            this.lista_produktów.Size = new System.Drawing.Size(157, 23);
+            this.lista_produktów.TabIndex = 14;
+            this.lista_produktów.UseWaitCursor = true;
+            this.lista_produktów.SelectedIndexChanged += new System.EventHandler(this.lista_produktów_SelectedIndexChanged);
+            // 
+            // dodaj
+            // 
+            this.dodaj.BackColor = System.Drawing.Color.Maroon;
+            this.dodaj.Font = new System.Drawing.Font("Ink Free", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dodaj.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.dodaj.Location = new System.Drawing.Point(273, 36);
+            this.dodaj.Name = "dodaj";
+            this.dodaj.Size = new System.Drawing.Size(75, 26);
+            this.dodaj.TabIndex = 13;
+            this.dodaj.TabStop = false;
+            this.dodaj.Text = "Dodaj";
+            this.dodaj.UseVisualStyleBackColor = false;
+            this.dodaj.UseWaitCursor = true;
+            this.dodaj.Click += new System.EventHandler(this.dodaj_Click);
+            // 
+            // Świat_Alkoholi_weryfikacja
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(647, 436);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.nrlokalu);
+            this.Controls.Add(this.adres);
+            this.Controls.Add(this.nazwisko);
+            this.Controls.Add(this.imię);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Świat_Alkoholi_weryfikacja";
+            this.Text = "Świat_Alkoholi_weryfikacja";
+            this.UseWaitCursor = true;
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox imię;
+        private System.Windows.Forms.TextBox nazwisko;
+        private System.Windows.Forms.TextBox adres;
+        private System.Windows.Forms.TextBox nrlokalu;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button wyślij;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RichTextBox koszyk;
+        private System.Windows.Forms.Label zamówienie;
+        private System.Windows.Forms.ComboBox lista_produktów;
+        private System.Windows.Forms.Button dodaj;
+    }
+}
+
---
+++ ŚwiatAlkoholi_MilenaBrowczuk/ŚwiatAlkoholi_MilenaBrowczuk/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ŚwiatAlkoholi_MilenaBrowczuk
+{
+    public partial class Świat_Alkoholi_weryfikacja : Form
+    {
+        public Świat_Alkoholi_weryfikacja()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void show()
+        {
+            panel1.Visible = true;
+        }
+        private void hide()
+        {
+            panel1.Visible = false;
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            int data = int.Parse(dateTimePicker1.Value.ToString("yyyyMMdd"));
+            int now = int.Parse(DateTime.Now.ToString("yyyyMMdd"));
+            int wiek = ((now - data) / 10000);
+            if (wiek >= 18)
+            {
+                panel1.Visible = true;
+                wyślij.Visible = true;
+            }
+            else
+            {
+                panel1.Visible = false;
+            }
+
+        }
+        private void dodaj_Click(object sender, EventArgs e)
+        {
+            if (lista_produktów.Text.Length != 0)
+            {
+                koszyk.Text += lista_produktów.Text;
+                koszyk.Text += ",  ";
+            }
+        }
+        private void lista_produktów_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+        }
+       
+        
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string path = @"C:\Users\mimil\Desktop\Uczelnia\Programowanie i testowanie aplikacji desktopowych\Formularze" + nazwisko.Text + "_" + imię.Text + ".txt";
+            string tekst = "Dane klienta: \r\n ";
+            tekst += "nazwisko: " + nazwisko.Text + " " + "imie: " + " " + imię.Text + "\r\n ";
+            tekst += "Dane kontaktowe: \r\n";
+            tekst += "Adres: " + adres.Text + "/" + nrlokalu.Text + "\r\n";
+            tekst += "Zamówienie: " + koszyk.Text + "\r\n";
+            tekst += "złożono: " + DateTime.Now.ToString("dddd, dd MM yyyy") + "\r\n";
+            if (imię.Text.Length == 0 || nazwisko.Text.Length == 0 || adres.Text.Length == 0 || nrlokalu.Text.Length == 0)
+            {
+                MessageBox.Show("Uzupełnij brakujące dane w formularzu!");
+            }
+            else
+            {
+                if (!File.Exists(path))
+                {
+                    File.WriteAllText(path, tekst);
+                }
+                else
+                {
+                    string zmiana = "";
+                    zmiana += "złożono: " + DateTime.Now.ToString("dddd, dd MM yyyy") + "\r\n";
+                    File.AppendAllText(path, zmiana);
+                }
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void koszyk_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
